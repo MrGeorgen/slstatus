427ec4549366a2073b96db2899cb83021add0dc5
MrGeorgen <moinl6162@gmail.com>
2020-12-15 18:48:40 +0100

    mrg fork

--- config.mk
+++ config.mk
@@ -7,12 +7,12 @@ VERSION = 0
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/include
+X11LIB = /usr/lib64
 
 # flags
 CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE
-CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Os
+CFLAGS   = -std=c99 -pedantic -Wall -Wextra -O2
 LDFLAGS  = -L$(X11LIB) -s
 LDLIBS   = -lX11
 
